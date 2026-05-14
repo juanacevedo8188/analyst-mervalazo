bac005db00b00f45cde423cdba1688557a3fdc65
juanacevedo8188 <bautiacevedo818@gmail.com>
2026-05-13 22:11:42 -0300

    Update analyst.js

--- netlify/functions/analyst.js
+++ netlify/functions/analyst.js
@@ -49,7 +49,7 @@ exports.handler = async function(event, context) {
     }
   };
 
-  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_KEY;
+  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + GEMINI_KEY;
 
   const res = await fetch(url, {
     method: "POST",
